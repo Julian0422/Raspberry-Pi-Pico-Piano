cca81e75fda331996049645cdb67f8429f9d4980
Julian0422 <106225286+Julian0422@users.noreply.github.com>
2022-07-12 17:15:57 +0800

    Update pitches.h

--- Raspberry Pi Pico Piano – Part 1 (88-key piano with only 14 buttons)/pitches.h
+++ Raspberry Pi Pico Piano – Part 1 (88-key piano with only 14 buttons)/pitches.h
@@ -1,1 +1,97 @@
+//frequencies for equal-tempered scale, A4=440Hz
+#define T_a1 55
+#define T_as1 58
+#define T_b1 61
 
+#define T_c2 65
+#define T_cs2 69
+#define T_d2 73
+#define T_ds2 77
+#define T_e2 82
+#define T_f2 87
+#define T_fs2 92
+#define T_g2 97
+#define T_gs2 103
+#define T_a2 110
+#define T_as2 116
+#define T_b2 123
+
+#define T_c3 130
+#define T_cs3 138
+#define T_d3 146
+#define T_ds3 155
+#define T_e3 164
+#define T_f3 174
+#define T_fs3 184
+#define T_g3 195
+#define T_gs3 207
+#define T_a3 220
+#define T_as3 233
+#define T_b3 246
+
+#define T_c4 261
+#define T_cs4 277
+#define T_d4 293
+#define T_ds4 311
+#define T_e4 329
+#define T_f4 349
+#define T_fs4 369
+#define T_g4 391
+#define T_gs4 415
+#define T_a4 440
+#define T_as4 366
+#define T_b4 493
+
+#define T_c5 523
+#define T_cs5 554
+#define T_d5 587
+#define T_ds5 622
+#define T_e5 659
+#define T_f5 698
+#define T_fs5 739
+#define T_g5 783
+#define T_gs5 830
+#define T_a5 880
+#define T_as5 932
+#define T_b5 987
+
+#define T_c6 1046
+#define T_cs6 1108
+#define T_d6 1174
+#define T_ds6 1244
+#define T_e6 1318
+#define T_f6 1396
+#define T_fs6 1479
+#define T_g6 1567
+#define T_gs6 1661
+#define T_a6 1760
+#define T_as6 1864
+#define T_b6 1975
+
+#define T_c7 2093
+#define T_cs7 2217
+#define T_d7 2349
+#define T_ds7 2489
+#define T_e7 2637
+#define T_f7 2793
+#define T_fs7 2959
+#define T_g7 3135
+#define T_gs7 3322
+#define T_a7 3520
+#define T_as7 3729
+#define T_b7 3951
+
+#define T_c8 4186
+#define T_cs8 4434
+#define T_d8 4698
+#define T_ds8 4978
+#define T_e8 5274
+#define T_f8 5587
+#define T_fs8 5919
+#define T_g8 6271
+#define T_gs8 6644
+#define T_a8 7040
+#define T_as8 7458
+#define T_b8 7920
+
+#define T_c9 7902
